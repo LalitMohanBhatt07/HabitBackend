96eac5c45716bdda08f4f26790627418562091ba
Lalit Mohan Bhatt <120781141+LalitMohanBhatt07@users.noreply.github.com>
2024-08-24 11:33:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,24 @@ const app = express();
 const port = 8000;
 
 // CORS Configuration
-app.use(cors({
-  // origin: 'https://66c95d259d77677365ca965d--zesty-kitten-96034b.netlify.app',
-  origin: '*',
+// app.use(cors({
+//   // origin: 'https://66c95d259d77677365ca965d--zesty-kitten-96034b.netlify.app',
+//   origin: '*',
 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
 
 // Middleware to handle preflight requests
-app.options('*', cors());
+// app.options('*', cors());
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://66c95d259d77677365ca965d--zesty-kitten-96034b.netlify.app'], 
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 // Other middleware
 app.use(express.json());
